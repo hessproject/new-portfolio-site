7ffdcb6719ff006a940151fdeb6ff2a7c9334793
hessproject <hessn1@duq.edu>
2016-11-23 15:14:50 -0500

    new settings

--- PortfolioWebsite/settings.py
+++ PortfolioWebsite/settings.py
@@ -23,7 +23,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'PortfolioWebsite.wsgi.application'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL')
     )
 }
 DATABASES['default'].update(db_from_env)
@@ -144,9 +144,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_TO_EMAIL = config('DEFAULT_TO_EMAIL')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_TO_EMAIL = os.environ.get('DEFAULT_TO_EMAIL')
 
 # Blog Settings
 ZINNIA_MARKUP_LANGUAGE = 'markdown'

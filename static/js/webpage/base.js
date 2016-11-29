c9f9bf7c7848bbd7fa52866777720aad45ee6dae
hessproject <hessn1@duq.edu>
2016-11-29 14:55:32 -0500

    static projects vs modal for size purposes

--- static/js/webpage/base.js
+++ static/js/webpage/base.js
@@ -2,14 +2,3 @@ $(window).on('load', function(){
     $('.preloader').hide();
     main();
 });
-
-$('#exampleModal').on('show.bs.modal', function(event){
-    var link = $(event.relatedTarget);
-    var title = link.data('title');
-    var videoSrc = link.data('video');
-    var modal = $(this);
-
-    modal.find('.modal-title').text(title);
-    modal.find('.modal-video').attr('src', videoSrc);
-    document.getElementById('exampleVideo').play();
-})
